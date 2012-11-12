89a47cd42ce91ca9383eec3a93e1d8d8e14709bb
Radosław Głombiowski <predoobcy@gmail.com>
2012-11-12 12:27:45 +0100

    Added lab project Jedzenie

---
+++ jedzenie/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jedzenie::Application.config.secret_token = 'cd6d88e017c1e23d5cbe4c663ac7c6bd1b82fa5ef671a2e374c03a6aae7df3d06fecfed588c030788526c318861b7c0355db985c1bbd83a2e39ec551b93d7ab1'
