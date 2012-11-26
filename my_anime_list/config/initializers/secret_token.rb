f0f3cd4fb2722eab9df3d85a42aa244e57d7ef02
Radosław Głombiowski <predoobcy@gmail.com>
2012-11-26 11:52:56 +0100

    Base

---
+++ my_anime_list/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyAnimeList::Application.config.secret_token = '83d0e3c4b172da9e8fc60be6e68e493ff4310d72d9c41d7c1d80fb460ce906068d40172722ddbc0e8b1e7fac927075b29e97f2c49d8aafb45fcfcf181fad82ed'
